800e778f45efd6d968949bed3f03cb8354cb4ca7
ChangMK <topplaza@gmail.com>
2017-10-06 14:39:40 +0800

    second->php->39

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 				<ul>
 					<li>14 different languages</li>
 					<li>DPI bug fixed</li>
-					<li>Php 5.4.18</li>
+					<li>Php 5.4.39</li>
 					<li>Httpd 2.4.6</li>
 					<li>PhpMyAdmin 4.0.4.2</li>
 					<li>MySQL 5.6.13</li>
